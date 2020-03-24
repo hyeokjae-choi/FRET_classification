78ffbdda92c539b74048328134bb06e377a96260
hyeokjae-choi <hyeokjae94@gmail.com>
2020-03-24 16:06:37 +0900

    gitignore requirements run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-for i in {1..100..1}; do
-    python main.py --train --ds_name transition --num_classes 2 \
-       --network stn3d --out_dir ./trained/stn3d/trans_notrans_$i
-done
-
-#python main.py --train --ds_name transition --num_classes 2 \
-#    --network stn3d  --out_dir ./trained/trans_notrans
-#
-#python main.py --test --ds_name transition --num_classes 3 \
-#    --network stn3d --out_dir ./trained/trans_notrans/trans_notrans_39
+python main.py --ds_name cy3_5_regression --project_name baseline
